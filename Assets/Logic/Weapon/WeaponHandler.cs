f4474e6bb0c881bd025e944ca499428200e590dd
Vladislav Kryuchkovsky <evakru2009@gmail.com>
2022-08-28 14:23:04 +0300

    Fix weapon holder

--- Assets/Logic/Weapon/WeaponHandler.cs
+++ Assets/Logic/Weapon/WeaponHandler.cs
@@ -16,6 +16,9 @@ namespace Logic.Weapon
         private Camera _camera;
         private WaitForSeconds _weaponSwitching;
         private int _weaponTypeIndex;
+        private int _weaponIndex;
+        private float _lastWeaponReload;
+        private bool _canShoot = true;
         private WeaponType _selectedWeaponType;
 
         private void Awake()
@@ -43,29 +46,49 @@ namespace Logic.Weapon
             _weaponTypeIndex = _weaponTypeIndex == 0 ? _weaponTypes.Count - 1 : _weaponTypeIndex - 1;
             SelectWeapon();
         }
+        
+        public void NextWeaponFromSelected() => _weaponIndex = _weaponIndex == _selectedWeapons.Count - 1 ? 0 : _weaponIndex + 1;
 
         private void Shoot(Vector3 inputPos)
         {
-            foreach (var weapon in _selectedWeapons)
+            var weapon = _selectedWeapons[_weaponIndex];
+            
+            if (!_canShoot || !weapon.CanShoot)
             {
-                if (!weapon.CanShoot)
-                {
-                    continue;
-                }
-                
-                var ray = _camera.ScreenPointToRay(inputPos);
-                var endPoint = Physics.Raycast(ray, out RaycastHit hit, _mask)
-                    ? hit.point
-                    : _camera.transform.position + ray.direction * weapon.FiringRange;
-                
-                weapon.Shoot(endPoint);
-                
                 return;
             }
+                
+            var ray = _camera.ScreenPointToRay(inputPos);
+            var endPoint = Physics.Raycast(ray, out RaycastHit hit, _mask)
+                ? hit.point
+                : _camera.transform.position + ray.direction * weapon.FiringRange;
+                
+            weapon.Shoot(endPoint);
+
+            if (weapon.ReloadTime > 0)
+            {
+                if (_lastWeaponReload != weapon.ReloadTime)
+                {
+                    _lastWeaponReload = weapon.ReloadTime;
+                    _weaponSwitching = new WaitForSeconds(_lastWeaponReload);
+                }
+                    
+                StartCoroutine(SwitchWeapon());
+            }
+                    
+            NextWeaponFromSelected(); 
+        }
+
+        private IEnumerator SwitchWeapon()
+        {
+            _canShoot = false;
+            yield return _weaponSwitching;
+            _canShoot = true;
         }
 
         private void SelectWeapon()
         {
+            _weaponIndex = 0;
             _selectedWeaponType = _weaponTypes[_weaponTypeIndex];
             _selectedWeapons = _weapons.Where(w => w.Type == _selectedWeaponType).ToList();
         }
