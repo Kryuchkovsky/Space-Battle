40755a68688e90cadd6178839d60fafdcf792eba
Vladislav Kryuchkovsky <evakru2009@gmail.com>
2022-08-31 03:46:43 +0300

    Add charge manager

---
+++ Assets/Logic/Services/ChargeManager.cs
@@ -0,0 +1,37 @@
+using Logic.Patterns;
+using Logic.Spaceships.Weapon;
+using UnityEngine;
+
+namespace Logic.Services
+{
+    public class ChargeManager : MonoBehaviour
+    {
+        public static ChargeManager Instance = null;
+
+        [SerializeField] private BlasterCharge _chargePrefab;
+
+        private ObjectPool<BlasterCharge> _chargePool;
+
+        private void Awake()
+        {
+            if (Instance == null) 
+            {
+                Instance = this;
+            } 
+            else if (Instance == this)
+            {
+                Destroy(gameObject);
+            }
+
+            _chargePool = new ObjectPool<BlasterCharge>(_chargePrefab, transform);
+        }
+
+        public BlasterCharge CreateEffectByType(Vector3 position, Quaternion rotation)
+        {
+            var charge = _chargePool.Take(position, rotation);
+            charge.Callback += _chargePool.Return;
+
+            return charge;
+        }
+    }
+}
